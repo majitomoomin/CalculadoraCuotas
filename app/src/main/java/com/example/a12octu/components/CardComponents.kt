4697ee968fafe1bcd8abd3dbf6800216ff715e64
majitomoomin <mariajoseportill2@gmail.com>
2024-10-14 23:13:51 -0600

    commit 3

--- app/src/main/java/com/example/a12octu/components/CardComponents.kt
+++ app/src/main/java/com/example/a12octu/components/CardComponents.kt
@@ -1,2 +1,14 @@
 package com.example.a12octu.components
 
+import androidx.copose.runtime.Composable
+
+@Composable
+fun InfoCard(title: String, info: Double, modifier: Modifier){
+    Card{
+        Column(
+
+        ){
+            Text(text = title, color = Color.Black, fontSize= 20.sp)
+        }
+    }
+}
